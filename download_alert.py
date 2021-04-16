d9628bf36ed83f843a4a4223e081555567280463
gautamsood15 <gauty22@gmail.com>
2021-04-16 13:12:22 +0530

    scrmbling important info

--- download_alert.py
+++ download_alert.py
@@ -2,8 +2,8 @@ import imaplib
 import email
 
 host = 'imap.outlook.com'                  # mailbox information
-username = 'gauty22@hotmail.com'
-password = 'Gs220797'
+username = 'gaasasf2@hasfoa.com'
+password = 'sdgfsdgsdgsdgsdg7'
 
 def get_inbox():
 
