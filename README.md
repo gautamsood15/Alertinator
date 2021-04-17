aa90111c0ef54125f62550e11857154e14dcbc05
gautamsood15 <gauty22@gmail.com>
2021-04-17 12:16:51 +0530

    select alert amils only code

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 
 #Alertinator
 
-It is a Project in python to download Microsoft service alert emails from an outlook mailbox.
-Then extract the alert information from the service alert mail. After that, it creates a custom,
-user friendly mail to clients of the specific project for which the service mail has arrived.
+It is a Project in python to download Microsoft service alert emails from an outlook mailbox (Thus marking them as
+read in the mailbox itself). Then extract the alert information from the service alert mail. After that, it creates 
+a custom, user friendly mail to clients of the specific project for which the service mail has arrived.
 
  
 ##Modules/Language Required for Project
 
 -> python 3.x -- language used to make the project
--> autopep8   -- Formats code automatically to PEP8 style guide		[autopep8 -i file_name], [pip install autopep8 --user]
+-> autopep8   -- Formats code automatically to PEP8 style guide		          [autopep8 -i file_name], [pip install autopep8 --user]
 -> os         -- Used to remove unwanted files from the project directory
 -> re         -- Used to search for text in alert info to get the proper data
--> win32com   -- Used to access outlook and to send mails using CreateItem() [pip install pypiwin32 --user]
+-> win32com   -- Used to access outlook and to send mails using CreateItem()  [pip install pypiwin32 --user]
 -> imaplib    -- Allows connection to IMAP4 server 
 -> email      -- Allow us to manage livrary messages, retrieve messages information, search messages 
 
+NOTE: For program to work properly, need to ensue that the mail application is open (for MS Outlook)
--- download_alert.py
+++ download_alert.py
@@ -14,34 +14,44 @@ def get_inbox():
 	_, search_data = mail.search(None, 'UNSEEN')          # searching for all unseen emails in inbox
 	my_message = []
 
-	for num in search_data[0].split():              # extracting the useful information from rest of the code 
+	for num in search_data[0].split():              # extracting the useful information from rest of the code for each unread mail 
 		email_data = {}
 		_, data = mail.fetch(num, '(RFC822)')
 		_, b = data[0]
 		
-		email_message = email.message_from_bytes(b)           # remove the bytes from the messages
+		email_message = email.message_from_bytes(b)           
 		
+#---------------------------------------------------------------------------------------------------------------------------------------
+	
+		if email_message['subject'] == 'Fwd: This is a test Email':    # check  for alert email from all thge unread email
 
-		for header in ['subject','to','from','date']:              # extract subject, to, from, data from the unread mails
-			# print("{}: {}".format(header,email_message[header]))
-			email_data[header] = email_message[header]
+			#for header in ['from', 'date', 'to', 'subject']:              # extract subject, to, from, data from the unread mails
+				#pass
+				#print("{}: {}".format(header,email_message[header]))
+				#email_data[header] = email_message[header]
 
-		for part in email_message.walk():                         # get the body of the mails
-			if part.get_content_type() == "text/plain":             # get body of mails if mail is text type
-				body = part.get_payload(decode=True)
-				email_data['body'] = body.decode()
+				
+				
+
+			for part in email_message.walk():                         # get the body of the mails
+				if part.get_content_type() == "text/plain":             # get body of mails if mail is text type
+					body = part.get_payload(decode=True)
+					email_data['body'] = body.decode()
 
 
-			elif part.get_content_type() == "text/html":              # get body of mails if mail is html type
-				html_body = part.get_payload(decode=True)
-				email_data['html_body'] = html_body.decode()
-		
+				elif part.get_content_type() == "text/html":              # get body of mails if mail is html type
+					html_body = part.get_payload(decode=True)
+					email_data['html_body'] = html_body.decode()
+
+		else:
+			print("other mail")
+			
 		my_message.append(email_data)
 	return my_message
 
 
 
-#                         code exection
+# ------------------------  Code Execution -------------------------------------------------
 
 my_inbox = get_inbox()
 print(my_inbox)
