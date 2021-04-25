595e47c1cd8e0ceb4eb48b3c92faae7a4b89f5b1
gautamsood15 <gauty22@gmail.com>
2021-04-25 13:30:37 +0530

    organizing all into functions

--- parse_text.py
+++ parse_text.py
@@ -4,69 +4,82 @@ import win32com.client as client
 
 # adding header to the alert info 
 
-alert_info = open('alert_info.txt', 'a')
-alert_info.write('                                     Microsoft Service Degradation - Alert\n\n\n')
-alert_info.close()
+def add_header():
+
+	alert_info = open('alert_info.txt', 'a')
+	alert_info.write('\n                                     Microsoft Service Degradation - Alert\n\n\n')
+	alert_info.close()
+
+	return
+
 
 
 # retrieving message from service alert
 
-
-with open('service_alert.txt') as input_file:
+def alert_parser():
 	
-	file_content = input_file.readlines()
+	with open('service_alert.txt') as input_file:
+	
+		file_content = input_file.readlines()
 
-# extracting alert info from service alert
+	# extracting alert info from service alert
 
-	for i, line in enumerate(file_content):
+		for i, line in enumerate(file_content):
 
-		if i == 13:
-			if line == 'Microsoft 365 suite service alert\n':
-				is_office_alert = True
-			else:
-				is_office_alert = False
+			if i == 13:
+				if line == 'Microsoft 365 suite service alert\n':
+					is_office_alert = True
+				else:
+					is_office_alert = False
 
-		if i == 20:
-			if line == 'Service Degradation\n':
-				is_service_degradation = True 
-			else:
-				is_service_degradation = False
+			if i == 20:
+				if line == 'Service Degradation\n':
+					is_service_degradation = True 
+				else:
+					is_service_degradation = False
 
-		if i == 18:
-			alert_id = line
-
-
-		if 44 > i > 21:
-			with open('alert_info.txt', 'a') as alert_info:
-				alert_info.write('\t'+line)
+			if i == 18:
+				alert_id = line
 
 
-# delete service alert 
+			if 44 > i > 21:
+				with open('alert_info.txt', 'a') as alert_info:
+					alert_info.write('\t'+line)
 
-os.remove("service_alert.txt")
+
+	# delete service alert 
+
+	os.remove("service_alert.txt")
+
+	return is_service_degradation, is_office_alert, alert_id
+
+
+
 
 
 # Adding signature to alert info
 
+def add_signature():
 
-alert_info = open('alert_info.txt', 'a')
+	with open('alert_info.txt', 'a') as alert_info:
 
-alert_info.write('\n\n')
-alert_info.write('Thanks and Regards,\n')
-alert_info.write('Gautam Sood,\n')
-alert_info.write('Messaging Team\n')
-alert_info.write('Accenture Services Private Limited\n')
-alert_info.write('Email: gautam.a.sood@accenture.com\n')
-
-alert_info.close()
-
+		alert_info.write('\n\n')
+		alert_info.write('Thanks and Regards,\n')
+		alert_info.write('Gautam Sood,\n')
+		alert_info.write('Messaging Team\n')
+		alert_info.write('Accenture Services Private Limited\n')
+		alert_info.write('Email: gautam.a.sood@accenture.com\n')
 
 
 
-# Check if the service alert is valid for messaging team to send to client
 
-def is_validated(office_alert, service_degradation):
+#  validate for messaging team to send to client
+
+
+def is_validated():
 	
+	office_alert, service_degradation, dummy_alert_id = alert_parser()
+
 	if office_alert == False:
 		os.remove("alert_info.txt")
 		print("False Alert\n")
@@ -83,24 +96,55 @@ def is_validated(office_alert, service_degradation):
 
 	return
 
-is_validated(is_office_alert, is_service_degradation)
 
 
 
 
-# Send email script 
 
-outlook = client.Dispatch("Outlook.Application")
-message = outlook.CreateItem(0)
-message.Display()
+# Choose between the projects to send 
 
-message.To = "shailsood15@gmail.com; gauty22@gmail.com"
-message.CC = "gauty22@hotmail.com; gauty@gautamsood.in"
-
-message.Subject = alert_id + " - M365 Service Health Notification"
-message.body = "This is a test case"
-
-message.Save()
-message.Send()
+def project_selection():
+	pass
+	
 
 
+
+
+
+
+
+# Send email to the clients 
+
+
+def send_email():
+	
+	outlook = client.Dispatch("Outlook.Application")
+	message = outlook.CreateItem(0)
+	message.Display()
+
+	message.To = "shailsood15@gmail.com; gauty22@gmail.com"
+	message.CC = "gauty22@hotmail.com; gauty@gautamsood.in"
+
+
+	alert_id, is_office_alert, is_service_degradation = alert_parser()
+
+	message.Subject = alert_id + " - M365 Service Health Notification"
+	message.body = "This is a test case"
+
+	message.Save()
+	message.Send()
+
+
+
+
+
+
+#------------------------  Code Execution -------------------------------------------------
+
+add_header()
+is_validated()
+add_signature()
+
+
+
+send_email()
