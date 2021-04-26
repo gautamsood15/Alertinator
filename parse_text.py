fff0fb54aa370654d0b2fdd8633c42312df97c09
gautamsood15 <gauty22@gmail.com>
2021-04-26 10:57:34 +0530

    Reading and parsing alert info from service alert text file

--- parse_text.py
+++ parse_text.py
@@ -1,13 +1,35 @@
 # copy Alert info data from the text file to another file 
 
-with open('Test Email.txt') as file:
-	file_content = file.readline()
-	print (file_content, end='')
-
-	file_content = file.readline()
-	print (file_content, end='')
 
 
+with open('Test Email.txt') as input_file:
+	
+	file_content = input_file.readlines()
+
+	for i, line in enumerate(file_content):
+
+		if i == 13:
+			if line == 'Microsoft 365 suite service alert\n':
+				is_office_alert = True
+			else:
+				is_office_alert = False
+
+		if i == 20:
+			if line == 'Service Degradation\n':
+				is_service_degradation = True 
+			else:
+				is_service_degradation = False
 
 
-file.close()
+		if 44 > i > 21:
+			with open('alert_info.txt', 'a') as alert_info:
+				alert_info.write(line)
+
+	print(is_service_degradation)
+	print(is_office_alert)
+
+
+
+
+
+
