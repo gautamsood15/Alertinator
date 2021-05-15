06e42e5db49b13e7f0a4349110ae1665cc241427
gautamsood15 <gauty22@gmail.com>
2021-05-15 12:19:26 +0530

    Adding signature and header to slert info

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Modules required 
+#Modules required 
 
-1. python 3                 #Download and install
-2. pypiwin32 --> win32com   #pip install pypiwin32 --user
+1. python 3                 ###Download and install
+2. pypiwin32 --> win32com   ###pip install pypiwin32 --user
 3. 
--- parse_text.py
+++ parse_text.py
@@ -1,10 +1,22 @@
-# copy Alert info data from the text file to another file 
+
 import os
+
+
+# adding header to the alert info 
+
+alert_info = open('alert_info.txt', 'a')
+alert_info.write('                                     Microsoft Service Degradation - Alert\n\n\n')
+alert_info.close()
+
+
+# retrieving data from service alert
 
 
 with open('service_alert.txt') as input_file:
 	
 	file_content = input_file.readlines()
+
+# extracting alert info from service alert
 
 	for i, line in enumerate(file_content):
 
@@ -25,13 +37,29 @@ with open('service_alert.txt') as input_file:
 			with open('alert_info.txt', 'a') as alert_info:
 				alert_info.write(line)
 
+# vairables to check if the service alert is valid for messaging team to send to client
+
 	print(is_service_degradation)
 	print(is_office_alert)
+
+
+# delete service alert 
 
 os.remove("service_alert.txt")
 
 
+# Adding signature to alert info
 
 
+alert_info = open('alert_info.txt', 'a')
+
+alert_info.write('\n\n')
+alert_info.write('Thanks and Regards,\n')
+alert_info.write('Gautam Sood,\n')
+alert_info.write('Messaging Team\n')
+alert_info.write('Accenture Services Private Limited\n')
+alert_info.write('Email: gautam.a.sood@accenture.com\n')
+
+alert_info.close()
 
 
